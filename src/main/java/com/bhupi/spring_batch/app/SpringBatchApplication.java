0c251f8a30d7555969f195c78b147f0d274e2458
Bhupendra Singh <bhupi29491@gmail.com>
2024-11-03 18:28:56 +0200

    Added condition Flow logic for the job.

--- src/main/java/com/bhupi/spring_batch/app/SpringBatchApplication.java
+++ src/main/java/com/bhupi/spring_batch/app/SpringBatchApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBatchApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBatchApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchApplication.class, args);
+        System.out.println("This is a Spring Batch Program..!!");
+    }
 
 }
--- src/main/java/com/bhupi/spring_batch/app/config/BatchConfig.java
+++ src/main/java/com/bhupi/spring_batch/app/config/BatchConfig.java
@@ -5,29 +5,69 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class BatchConfig {
 
-    private final PrintHelloTask printHelloTask;
-
-    public BatchConfig(PrintHelloTask printHelloTask) {
-        this.printHelloTask = printHelloTask;
-    }
-
     @Bean
-    public Step step1(JobRepository jobRepository, Tasklet task, PlatformTransactionManager transactionManager) {
-        return new StepBuilder("step1", jobRepository).tasklet(task, transactionManager)
+    @Primary
+    public Step step1(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("step1", jobRepository).tasklet((contribution, chunkContext) -> {
+                                                          System.out.println("Step1 is executed..!!");
+                                                          return RepeatStatus.FINISHED;
+                                                      }, transactionManager)
                                                       .build();
     }
 
     @Bean
-    public Job firstJob(JobRepository jobRepository, Step step, PlatformTransactionManager transactionManager) {
-        return new JobBuilder("job1", jobRepository).start(step1(jobRepository, printHelloTask, transactionManager))
+    public Step step2(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("step2", jobRepository).tasklet((contribution, chunkContext) -> {
+                                                          boolean isFailure = true;
+                                                          if (isFailure) {
+                                                              throw new Exception("Test Exception");
+                                                          }
+                                                          System.out.println("Step2 is executed..!!");
+                                                          return RepeatStatus.FINISHED;
+                                                      }, transactionManager)
+                                                      .build();
+    }
+
+    @Bean
+    public Step step3(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("step3", jobRepository).tasklet((contribution, chunkContext) -> {
+                                                          System.out.println("Step3 is executed..!!");
+                                                          return RepeatStatus.FINISHED;
+                                                      }, transactionManager)
+                                                      .build();
+    }
+
+    @Bean
+    public Step step4(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("step4", jobRepository).tasklet((contribution, chunkContext) -> {
+                                                          System.out.println("Step4 is executed..!!");
+                                                          return RepeatStatus.FINISHED;
+                                                      }, transactionManager)
+                                                      .build();
+    }
+
+
+    @Bean
+    public Job firstJob(JobRepository jobRepository, PlatformTransactionManager transactionManager) throws Exception {
+        return new JobBuilder("job1", jobRepository).start(step1(jobRepository, transactionManager))
+                                                    .on("COMPLETED")
+                                                    .to(step2(jobRepository, transactionManager))
+                                                    .from(step2(jobRepository, transactionManager))
+                                                    .on("COMPLETED")
+                                                    .to(step3(jobRepository, transactionManager))
+                                                    .from(step2(jobRepository, transactionManager))
+                                                    .on("*")
+                                                    .to(step4(jobRepository, transactionManager))
+                                                    .end()
                                                     .build();
     }
 
