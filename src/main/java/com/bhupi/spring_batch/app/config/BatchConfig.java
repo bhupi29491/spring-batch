7ed4d51d5d20408a1ef2da4f1ecd8d5034039a8e
Bhupendra Singh <bhupi29491@gmail.com>
2024-11-11 21:55:24 +0200

    Implemented Flow and combined two steps in the job.

--- src/main/java/com/bhupi/spring_batch/app/config/BatchConfig.java
+++ src/main/java/com/bhupi/spring_batch/app/config/BatchConfig.java
@@ -5,14 +5,15 @@ import com.bhupi.spring_batch.app.listener.MyStepExecutionListener;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepExecutionListener;
+import org.springframework.batch.core.job.builder.FlowBuilder;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.job.flow.JobExecutionDecider;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
@@ -29,7 +30,6 @@ public class BatchConfig {
     }
 
     @Bean
-    @Primary
     public Step step1(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
         return new StepBuilder("step1", jobRepository).tasklet((contribution, chunkContext) -> {
                                                           System.out.println("Step1 is executed..!!");
@@ -41,10 +41,6 @@ public class BatchConfig {
     @Bean
     public Step step2(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
         return new StepBuilder("step2", jobRepository).tasklet((contribution, chunkContext) -> {
-                                                          boolean isFailure = false;
-                                                          if (isFailure) {
-                                                              throw new Exception("Test Exception");
-                                                          }
                                                           System.out.println("Step2 is executed..!!");
                                                           return RepeatStatus.FINISHED;
                                                       }, transactionManager)
@@ -70,17 +66,37 @@ public class BatchConfig {
                                                       .build();
     }
 
+    @Bean
+    public Flow flow1(Step step3, Step step4) {
+        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flow1");
+        flowBuilder.start(step3)
+                   .next(step4)
+                   .end();
+        return flowBuilder.build();
+    }
+
+
+//    @Bean
+//    public Job firstJob(JobRepository jobRepository, PlatformTransactionManager transactionManager) throws Exception {
+//        return new JobBuilder("job1", jobRepository).start(step1(jobRepository, transactionManager))
+//                                                    .on("COMPLETED")
+//                                                    .to(jobExecutionDecider())
+//                                                    .on("TEST_STATUS")
+//                                                    .to(step2(jobRepository, transactionManager))
+//                                                    .from(jobExecutionDecider())
+//                                                    .on("*")
+//                                                    .to(step3(jobRepository, transactionManager))
+//                                                    .end()
+//                                                    .build();
+//    }
+
 
     @Bean
-    public Job firstJob(JobRepository jobRepository, PlatformTransactionManager transactionManager) throws Exception {
-        return new JobBuilder("job1", jobRepository).start(step1(jobRepository, transactionManager))
+    public Job firstJob(JobRepository jobRepository, PlatformTransactionManager transactionManager, Step step1, Step step2, Flow flow1) throws Exception {
+        return new JobBuilder("job1", jobRepository).start(step1)
+                                                    .next(step2)
                                                     .on("COMPLETED")
-                                                    .to(jobExecutionDecider())
-                                                    .on("TEST_STATUS")
-                                                    .to(step2(jobRepository, transactionManager))
-                                                    .from(jobExecutionDecider())
-                                                    .on("*")
-                                                    .to(step3(jobRepository, transactionManager))
+                                                    .to(flow1)
                                                     .end()
                                                     .build();
     }
